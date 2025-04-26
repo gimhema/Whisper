f8f7cb62ef07347c92d3faa2582dd6763653fecd
kimmo <hemakim0215@naver.com>
2025-04-26 02:14:37 +0000

    .

--- whisper/pkg/node/whisper_node.go
+++ whisper/pkg/node/whisper_node.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"whisper/pkg/common"
 	"os"
+	"net"
 )
 
 type Node struct {
@@ -18,10 +19,21 @@ func CreateNode() *Node {
 }
 
 func (node *Node) Run() {
-	fmt.Println("Run Message Node")
+	fmt.Println("Run Message Broker")
+
+	node.tcpConn.SetMessageHandler(node.handleMessage)
 
 	if err := node.tcpConn.Run(); err != nil {
 		fmt.Println("Server error:", err)
 		os.Exit(1)
 	}
+}
+
+func (node *Node) handleMessage(conn net.Conn, data []byte) {
+	// 예: 메시지를 처리하거나 라우팅
+	msg := string(data)
+	fmt.Printf("Broker received: %s\n", msg)
+
+	// 예: 클라이언트에 응답 보내기
+	conn.Write([]byte("ACK: " + msg))
 }
